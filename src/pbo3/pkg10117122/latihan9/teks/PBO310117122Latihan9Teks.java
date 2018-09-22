eaa5a42af5e00381019dbe3ebd094eede2cc08ca
BonaventuraAldino <aldysenda99@gmail.com>
2018-09-23 01:19:57 +0700

    PBO3-10117122-Latihan9-Teks

---
+++ src/pbo3/pkg10117122/latihan9/teks/PBO310117122Latihan9Teks.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117122.latihan9.teks;
+
+/**
+ *
+ * @author lenovo
+ * 
+ * NAMA  : BONAVENTURA ALDINO SENDA SENI
+ * KELAS : PBO-3
+ * NIM   : 10117122
+ * 
+ */
+public class PBO310117122Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab ='\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Chart: " + tab + "berisi "+ c);
+    }
+}
